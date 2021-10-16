06a7ea03d918b31c88e6a1dd5614de01e879a668
Jeronymo Dalapicolla <34402608+jdalapicolla@users.noreply.github.com>
2021-10-16 20:24:19 -0300

    Update Passo05 - Filtragem Pontos de Ocorrencia.R

--- Passo05 - Filtragem Pontos de Ocorrencia.R
+++ Passo05 - Filtragem Pontos de Ocorrencia.R
@@ -20,29 +20,39 @@
 library(rgdal)
 library(raster)
 library(dismo)
+library(rangeBuilder)
+
 
 #delimitar uma projecao espacial para lat/long e para UTM:
-longlat_WGS = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-UTM_proj = CRS("+proj=utm +zone=22 +south +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+#Verificar a projeção 
+rgdal::rgdal_extSoftVersion()
 
-#ou criar uma a partir de codigo EPGS
-A = make_EPSG() 
+#Para GDAL >3 e PROJ >6
+(LL_prj <- CRS("+init=epsg:4326"))
+cat(strwrap(gsub(",", ", ", (comment(LL_prj)))), sep="\n")
+LL_prj
+
+(UTM_prj <- CRS("+init=epsg:5383"))
+cat(strwrap(gsub(",", ", ", (comment(UTM_prj)))), sep="\n")
+UTM_prj
+
+
+#ou  GDAL <3 e PROJ <6
+A = rgdal::make_EPSG() 
 LL_prj = as.character(subset(A, A$code=="4326")[3]) 
 UTM_prj = as.character(subset(A, A$code=="5383")[3]) 
 
 
 
-
-
 ####################### 2. CARREGAR OS ARQUIVOS NECESSARIOS ########################
 # Carregar os pontos de ocorrencia:
-pontos_brutos = read.csv("./pontos/psimonsi.csv")
+pontos_brutos = read.csv("./simonsi/points/points_raw_sim.csv")
 head(pontos_brutos)
 str(pontos_brutos)
 
 # Carregar a camada ambiental em 'asc' ja editada para servir como modelo
-variavel = raster("./asc_presente/wc2.1_30s_bio_1.asc")
-crs(variavel) = longlat_WGS
+variavel = raster("./asc_presente/annualPET.asc")
+crs(variavel) = LL_prj
 variavel
 plot(variavel)
 
@@ -52,13 +62,13 @@ plot(variavel)
 
 ########################## 3. REMOVER PONTOS DUPLICADOS ############################
 #numero de pontos brutos:
-length(pontos_brutos[, 1]) #148
+length(pontos_brutos[, 1]) #164
 
 #remover duplicados
-pontos_unicos = pontos_brutos[!duplicated(pontos_brutos[c("Long","Lat")]), ]
+pontos_unicos = pontos_brutos[!duplicated(pontos_brutos[c("longitude","latitude")]), ]
 
 #numero de pontos unicos:
-length(pontos_unicos[, 1]) #55
+length(pontos_unicos[, 1]) #60
 
 
 
@@ -66,31 +76,32 @@ length(pontos_unicos[, 1]) #55
 
 ##################### 4. REMOVER PONTOS FORA DA AREA DE ESTUDO #####################
 #numero de pontos unicos:
-length(pontos_unicos[, 1]) #55
+length(pontos_unicos[, 1]) #60
 
 #selecionar apenas as colunas de lat e long:
 names(pontos_unicos)
-ocorrencia = pontos_unicos[,3:4] # lon/lat columns
+ocorrencia = pontos_unicos[,2:3] # lon/lat columns
 head(ocorrencia)
 str(ocorrencia)
 
 #adicionar uma projecao
-coordinates(ocorrencia) = ~Long+Lat #nome das colunas.
-crs(ocorrencia) =  longlat_WGS
+coordinates(ocorrencia) = ~longitude+latitude #nome das colunas.
+crs(ocorrencia) =  LL_prj
 
 #extrair os valores da camada
 valores = extract(variavel, ocorrencia)
 head(valores)
 
-#achar as posições onde não há valor, é um ponto fora da camada. NoData nos arquivos '.asc' é normalmente '-9999', mas você pode alterar o valor
+#achar as posições onde não há valor, é um ponto fora da camada. NoData nos arquivos '.asc' é normalmente '-9999', mas você pode ser NA alterar o valor. testar os dois.
 i = which(valores != "-9999")
+
+i = which(valores != "NA")
 i #lines in the point_raw
 
 #update the points raw and create a SpatialPoints for ocorrence points.
 pontos_unicos_area = pontos_unicos[i,]
 #numero de pontos restantes:
-length(pontos_unicos_area[, 1]) #51
-
+length(pontos_unicos_area[, 1]) #56
 
 
 
@@ -98,30 +109,20 @@ length(pontos_unicos_area[, 1]) #51
 ####### 5. REMOVER VIES AMOSTRAL QUE PODE LEVAR A AUTOCORRELACAO ESPACIAL ##########
 #transformar os pontos em SpatialPoints
 names(pontos_unicos_area)
-ocorrencia = pontos_unicos_area[,3:4] # lon/lat columns
-coordinates(ocorrencia) = ~Long+Lat #nome das colunas.
-crs(ocorrencia) = longlat_WGS
+ocorrencia = pontos_unicos_area[,2:3] # lon/lat columns
+coordinates(ocorrencia) = ~longitude+latitude #nome das colunas.
 
-#criar um buffer de 10Km ao redor dos pontos
-buffer = circles(ocorrencia, d = 10000, lonlat=TRUE) #d é o raio do circulo em metros
-plot(buffer)
-class(buffer)
 
-#converter os círculos em polígonos
-buffer = polygons(buffer)
-#rasterizar os circulos
-buffer= rasterize(buffer, variavel)
-
-#selecionar 1 ponto por cada circulo
-sel = gridSample(ocorrencia, buffer, n=1)
+#selecionar pontos que tenham 10Km de distância de cada
+sel = filterByProximity(ocorrencia@coords, dist = 10, mapUnits = FALSE, returnIndex = FALSE)
 
 #verificar o numero de pontos restantes:
-length(pontos_unicos_area[,1]) #51
-length(sel[,1]) #47
+length(pontos_unicos_area[,1]) #56
+length(sel[,1]) #41
 
 #salvar os pontos de ocorrencia corrigidos
 sel = as.data.frame(sel)
-write.csv(sel, "./pontos/psimonsi_corrigido.csv", row.names = FALSE)
+write.csv(sel, "./simonsi/points/points_filtered_sim.csv", row.names = FALSE)
 
 
 #END
